3d5697758ba7687c50134bc710bdb25af6a02c43
HalitCanbaz <107313207+HalitCanbaz@users.noreply.github.com>
2023-12-14 10:32:57 +0300

    Crm

---
+++ CrmApp/Models/Categories.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace CrmApp.Models
+{
+    public class Categories
+    {
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string CategoryName { get; set; }
+
+        public ICollection<VarlikCategories> Varlikcategories { get; set; } = new HashSet<VarlikCategories>();
+    }
+}
